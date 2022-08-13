add9a8b99f1bed421659d56a3120bbdbe536b730
Amine Cheraitia <amine-cheraitia@hotmail.com>
2022-08-13 02:34:12 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,35 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+	<div class="container">
+		<div class="search-b">
+			<input type="text" id="searchbar" placeholder="Search..." />
+		</div>
+		<div class="inner-container">
+			<h4>Ville</h4>
+			<p>Date 14/03/1991</p>
+			<div>température</div>
+			<p>etat</p>
+		</div>
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+	name: "App",
+	data() {
+		return {
+			api_key: "a8f12504ac1bcb5b5bddafaf8e3ddc7c",
+		};
+	},
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
 }
 </style>
